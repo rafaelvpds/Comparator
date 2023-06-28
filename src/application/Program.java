2a5c11203e36f9c5b9fe80d0b2c1f1980fd3e36d
rafaelvpds <33781893+rafaelvpds@users.noreply.github.com>
2023-06-28 11:14:43 -0300

    implementando a classe MyComparator

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import entities.Product;
@@ -15,8 +16,17 @@ public class Program {
 		lista.add(new Product("tv", 900.00));
 		lista.add(new Product("notebook", 900.00));
 		lista.add(new Product("tablet", 900.00));
-		// interface funcional: interface que tem apenas um metodo abstrado
-		lista.sort(new MyComparator());
+		// implementando em uma classe anonima:
+
+		Comparator<Product> comp = new Comparator<Product>() {
+
+			@Override
+			public int compare(Product p1, Product p2) {
+
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+		};
+		lista.sort(comp);
 
 		for (Product p : lista) {
 			System.out.println(p);
